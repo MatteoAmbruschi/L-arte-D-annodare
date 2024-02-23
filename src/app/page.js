cddb50ec571cd9779124aae8b7f34333504e3f91
Matteo Ambruschi <matteoambruschi10@gmail.com>
2024-02-23 18:39:38 +0100

    .

--- src/app/page.js
+++ src/app/page.js
@@ -3,11 +3,6 @@ import Head from 'next/head'
 export default function Home() {
   return (
     <main>
-      <Head>
-        <title>My page title</title>
-        <meta property="og:title" content="My page title" key="title" />
-      </Head>
-
       <h1>L&apos;ARTE D&apos;ANNODARE</h1>
       {/* <Main></Main> */}
     </main>
--- src/componens/menu/Menu.jsx
+++ src/componens/menu/Menu.jsx
@@ -7,18 +7,27 @@ const Menu = () => {
     return (
         <nav className={styles.nav}>
             <div className={styles.navContainer}>
+            
+            <Link href="/" className={styles.linkMenu}><Image src='/logo.png' width={240.48} height={34.00} alt="logo l'arte d'annodare" /></Link>
+            
                 <ul className={styles.navMenu}>
-                    <Link href="/" className={styles.linkMenu}><Image src='/logo.png' width={240.48} height={34.00} /></Link>
-                <li>
-                    <Link href="/lavaggio-tappeti-bergamo" className={styles.linkMenu}>LAVAGGIO TAPPETI</Link>
-                </li>
-                <li>
-                    <Link href="/tappeti-persiani" className={styles.linkMenu}>TAPPETI PERSIANI</Link>
-                </li>
-                <li>
-                    <Link href="/contatti" className={styles.linkMenu}>CONTATTI</Link>
-                </li>
+                    <div className={styles.conteinerLinks}>
+                        <li>
+                            <Link href="/lavaggio-tappeti-bergamo" className={styles.linkMenu}>LAVAGGIO TAPPETI</Link>
+                        </li>
+                        <li>
+                            <Link href="/tappeti-persiani" className={styles.linkMenu}>TAPPETI PERSIANI</Link>
+                        </li>
+                        <li>
+                            <Link href="/contatti" className={styles.linkMenu}>CONTATTI</Link>
+                        </li>
+                    </div>
                 </ul>
+                
+                    <div className={styles.iconHamburger}>
+                        <span></span>
+                        <span></span>
+                    </div>
             </div>
         </nav>
 
