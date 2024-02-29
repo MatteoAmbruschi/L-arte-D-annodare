855036cb44383db9a64ff5c76ce1be9efdc6c871
Matteo Ambruschi <matteoambruschi10@gmail.com>
2024-02-29 11:35:38 +0100

    backup prima di inserire h4

--- src/app/page.js
+++ src/app/page.js
@@ -2,11 +2,13 @@ import Header from '@/componens/header/Header';
 import Head from 'next/head'
 
 export default function Home() {
+  const textHead= "<strong>L'arte d'annodare di Giampietro Ambruschi a Seriate (Bergamo),</strong> è un punto di riferimento a <strong>Bergamo e provincia per chi ama il tappeto orientale.</strong>Offriamo nel nostro negozio/laboratorio un'accurato <strong>servizio di vendita, restauro e lavaggio tappeti.</strong>"
+
   return (
     <>
-      <Header />
+      <Header imgHead={"/tappetiBergamo.jpg"} subTitle={'NEGOZIO DI TAPPETI'} title={"L'ARTE D'ANNODARE"} text={textHead} />
       <div className='mainContent'>
-        <h1>L&apos;ARTE D&apos;ANNODARE</h1>
+        <h2>I nostri servizi</h2>
       </div>
     </>
   );
--- src/componens/header/Header.jsx
+++ src/componens/header/Header.jsx
@@ -1,22 +1,29 @@
 import styles from './header.module.css'
 import Image from 'next/image'
 
-function Header() {
+function Header({imgHead, title, subTitle, text}) {
 
     return (
-
-      <div className={styles.container}>
         <section className={styles.containerHeader}>
-            <div className={styles.effectsOldSite}>
+            <div className={styles.img} style={{backgroundImage:`url(${imgHead})`}} />
                 <div className={styles.gradient}>
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1000 100" preserveAspectRatio="none">
-                        <path className={styles.shapeFill }d="M0,6V0h1000v100L0,6z"></path>
+                        <path className={styles.shapeFill}d="M0,6V0h1000v100L0,6z"></path>
                     </svg>
                 </div>
-            </div>
-        </section>
-        ciao
-    </div>      
+
+                <div className={styles.containerText}>
+                      <h3 className={styles.subTitle}>
+                            {subTitle}
+                      </h3>
+                      <h1>
+                        {title}
+                      </h1>
+                      <h4>
+                        {text}
+                      </h4>
+                </div>
+        </section>  
     )
 }
 
