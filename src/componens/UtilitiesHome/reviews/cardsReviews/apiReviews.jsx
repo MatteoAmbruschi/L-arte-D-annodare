60d49f923346a950bfdde4a66c4098b5655727d5
Matteo Ambruschi <matteoambruschi10@gmail.com>
2024-03-05 17:18:51 +0100

    .

--- src/componens/UtilitiesHome/reviews/cardsReviews/apiReviews.jsx
+++ src/componens/UtilitiesHome/reviews/cardsReviews/apiReviews.jsx
@@ -1,43 +1,39 @@
 'use client'
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 
+export async function getStaticProps() {
+  const apiKey = 'AIzaSyCIdnqck9F1laYdu4JQt1V3Mk_CVxSwoj8';
+  const placeID = 'ChIJv8DmMDhXgUcRHryN6sOp40Y';
+  let res = null;
 
-  export async function getStaticProps() {
-    const apiKey = 'AIzaSyCIdnqck9F1laYdu4JQt1V3Mk_CVxSwoj8';
-    const placeID = 'ChIJv8DmMDhXgUcRHryN6sOp40Y';
-    let res = null;
-  
-    const response = await fetch('/reviewsGoogle', {
-      method: "GET",
-      mode: "cors",
-      headers: new Headers({
-        "Content-Type": "application/json",
-      }),
-    });
+  try {
+    const response = await fetch(`https://maps.googleapis.com/maps/api/place/details/json?place_id=ChIJv8DmMDhXgUcRHryN6sOp40Y&fields=reviews&language=it&key=AIzaSyCIdnqck9F1laYdu4JQt1V3Mk_CVxSwoj8`);
+    res = await response.json();
+  } catch (error) {
+    console.log(error);
+  }
 
-    if (response.ok) {
-      const res = await response.json();
+  return {
+    props: {
+      res,
+    },
+  };
+}
+
+function MyReview({ res }) {
+  useEffect(() => {
+    if (res) {
       console.log(res);
     } else {
-      console.log(`Errore nella richiesta: ${response.statusText}`);
+      console.log("Dati della recensione non disponibili.");
     }
-  }
+  }, [res]);
 
-
-  function MyReview({ response }) {
-    useEffect(() => {
-      if (response) {
-        console.log(response);
-      } else {
-        console.log("Dati della recensione non disponibili.");
-      }
-    }, [response]);
-  
-    return (
-      <div>
-        funziona?
-      </div>
-    );
-  }
+  return (
+    <div>
+      funziona??
+    </div>
+  );
+}
 
 export default MyReview;
