48079731a411510aec5333796d1dc58754a9d893
Matteo Ambruschi <matteoambruschi10@gmail.com>
2024-03-05 16:48:59 +0100

    prova

--- src/componens/UtilitiesHome/reviews/cardsReviews/apiReviews.jsx
+++ src/componens/UtilitiesHome/reviews/cardsReviews/apiReviews.jsx
@@ -1,24 +1,31 @@
 'use client'
 import { useEffect, useState } from 'react';
 
-export async function getStaticProps() {
+
+  export async function getStaticProps() {
     const apiKey = 'AIzaSyCIdnqck9F1laYdu4JQt1V3Mk_CVxSwoj8';
     const placeID = 'ChIJv8DmMDhXgUcRHryN6sOp40Y';
+    let res = null;
   
-
-    const response = await fetch(`https://maps.googleapis.com/maps/api/place/details/json?place_id=ChIJv8DmMDhXgUcRHryN6sOp40Y&fields=reviews&language=it&key=AIzaSyCIdnqck9F1laYdu4JQt1V3Mk_CVxSwoj8`)
-    const json = await response.json();
-
-    console.log(json); 
+    try {
+      const response = await fetch(`https://maps.googleapis.com/maps/api/place/details/json?place_id=ChIJv8DmMDhXgUcRHryN6sOp40Y&fields=reviews&language=it&key=AIzaSyCIdnqck9F1laYdu4JQt1V3Mk_CVxSwoj8`)
+    
+      res = await response.json();
+    } catch (error) {
+      console.log(error);
+    }
+  
     return {
-      props: { json }
+      props: {
+        res,
+      },
     };
   }
 
 
   function MyReview({ json }) {
     useEffect(() => {
-      console.log(getStaticProps());  
+      console.log(json);  
     }, [json]);
   
     return (
