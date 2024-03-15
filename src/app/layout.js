415aa8545f10fbdcd1e48c244139cb43d090e3cd
Matteo Ambruschi <matteoambruschi10@gmail.com>
2024-03-15 13:12:34 +0100

    font dal css woff2

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,33 +4,6 @@ import Menu from "@/componens/menu/Menu";
 import Footer from "@/componens/footer/Footer"
 import { Analytics } from "@vercel/analytics/react"
 import { SpeedInsights } from '@vercel/speed-insights/next';
-import localFont from 'next/font/local'
-
-const TimesNewRoman = localFont({
-  src: [
-    {
-      path: 'TimesNewRoman/times-new-roman.ttf',
-      weight: 'normal',
-      style: 'normal',
-    },
-    {
-      path: 'TimesNewRoman/times-new-roman-italic.ttf',
-      weight: 'normal',
-      style: 'italic',
-    },
-    {
-      path: 'TimesNewRoman/times-new-roman-bold.ttf',
-      weight: 'bold',
-      style: 'normal',
-    },
-    {
-      path: 'TimesNewRoman/times-new-roman-bold-italic.ttf',
-      weight: 'bold',
-      style: 'italic',
-    },
-  ],
-})
-
 
 export const metadata = {
   title: "L'arte D'annodare | Tappeti orientali Bergamo, lavaggio e restauro a Seriate (Bergamo)",
@@ -40,7 +13,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="it">
-      <body className={TimesNewRoman.className}>
+      <body>
         <Menu />
             {children}
           <Footer />
