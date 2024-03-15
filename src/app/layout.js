658983b76e31be8fb1a1d19d6e0ddb23b198b9c6
Matteo Ambruschi <matteoambruschi10@gmail.com>
2024-03-15 17:16:26 +0100

    .

--- src/app/layout.js
+++ src/app/layout.js
@@ -10,7 +10,9 @@ export const metadata = {
     default: "L'arte D'annodare | Tappeti orientali Bergamo, lavaggio e restauro a Seriate (Bergamo)",
     template: "%s | L'arte D'annodare"
   },
-  description: "Punto di riferimento a Bergamo e provincia per chi ama il tappeto orientale. Offriamo vendita, restauro e lavaggio tappeti. Servizio di consulenza e preventivi gratuiti.",
+  description: {
+    default: "Punto di riferimento a Bergamo e provincia per chi ama il tappeto orientale. Offriamo vendita, restauro e lavaggio tappeti. Servizio di consulenza e preventivi gratuiti."
+  }
 };
 
 export default function RootLayout({ children }) {
