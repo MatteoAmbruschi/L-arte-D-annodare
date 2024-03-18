50b16fae57bcde4ec7a439f32d7dfd71c1c15b9a
Matteo Ambruschi <matteoambruschi10@gmail.com>
2024-03-18 11:49:50 +0100

    .

--- src/app/cookie/page.jsx
+++ src/app/cookie/page.jsx
@@ -5,9 +5,9 @@ function Cookie (){
     return (
         <div className={`mainContent`}>
             <h1>Cookies</h1>
-            <p>
-                <script id="CookieDeclaration" src="https://consent.cookiebot.com/1a5e81ef-240c-49e0-8b31-85175b14464d/cd.js" type="text/javascript" async></script>
-            </p>
+            <div>
+                {<script id="CookieDeclaration" src="https://consent.cookiebot.com/1a5e81ef-240c-49e0-8b31-85175b14464d/cd.js" type="text/javascript" async></script>}
+            </div>
             
                 <Script id="Cookiebot" src="https://consent.cookiebot.com/uc.js" data-cbid="1a5e81ef-240c-49e0-8b31-85175b14464d" data-blockingmode="auto" type="text/javascript"></Script>
         </div>
