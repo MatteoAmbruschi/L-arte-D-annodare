ff5f2feb2cc2620e046075c39b825865e0229961
Matteo Ambruschi <matteoambruschi10@gmail.com>
2024-03-18 11:40:51 +0100

    cookies

--- src/app/cookie/page.jsx
+++ src/app/cookie/page.jsx
@@ -1,11 +1,12 @@
 import Script from 'next/script'
+
 function Cookie (){
 
     return (
-        <div className="mainContent">
+        <div className={`mainContent ${styles.cookies}`}>
             <h1>Cookies</h1>
             <p>
-                <Script id="CookieDeclaration" src="https://consent.cookiebot.com/1a5e81ef-240c-49e0-8b31-85175b14464d/cd.js" type="text/javascript" async></Script>
+                <script id="CookieDeclaration" src="https://consent.cookiebot.com/1a5e81ef-240c-49e0-8b31-85175b14464d/cd.js" type="text/javascript" async></script>
             </p>
             
                 <Script id="Cookiebot" src="https://consent.cookiebot.com/uc.js" data-cbid="1a5e81ef-240c-49e0-8b31-85175b14464d" data-blockingmode="auto" type="text/javascript"></Script>
