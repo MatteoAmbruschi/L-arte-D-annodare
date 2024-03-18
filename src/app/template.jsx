40f40849f44a0443ba974d891b12475357e88557
Matteo Ambruschi <matteoambruschi10@gmail.com>
2024-03-18 12:00:53 +0100

    prova

--- src/app/template.jsx
+++ src/app/template.jsx
@@ -1,32 +1,32 @@
-/* 'use client'
+'use client'
 import { LazyMotion, domAnimation, m } from "framer-motion";
 import { ReactLenis } from '@studio-freight/react-lenis';
-import { useState, useEffect } from 'react' */
+import { useState, useEffect } from 'react'
 
 export default function Template({ children }) {
-/*     const [isMobile, setIsMobile] = useState(false);
+    const [isMobile, setIsMobile] = useState(false);
 
     useEffect(() => {
         setIsMobile(window.innerWidth <= 768);
-    }, []); */
+    }, []);
 
     return (
-      /*   <LazyMotion features={domAnimation}>
+        <LazyMotion features={domAnimation}>
             <m.div
-                initial={{ opacity: 0 }}
+                initial={{ opacity: 0.05 }}
                 animate={{ opacity: 1 }}
                 transition={{ ease: 'easeInOut', duration: 0.45 }}
             >
 
-            {isMobile ? ( */
+            {isMobile ? (
                     children
-/*                 ) : (
+                ) : (
                     <ReactLenis root>
                         {children}
                     </ReactLenis>
             )}
             
             </m.div>
-        </LazyMotion> */
+        </LazyMotion>
     );
 }
