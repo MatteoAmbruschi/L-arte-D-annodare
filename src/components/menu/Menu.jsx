ae279cac570da35427af0f019acc7998d2afeaf7
Matteo Ambruschi <matteoambruschi10@gmail.com>
2024-03-26 21:59:36 +0100

    si logo

--- src/components/menu/Menu.jsx
+++ src/components/menu/Menu.jsx
@@ -34,7 +34,7 @@ const Menu = () => {
             <div className={styles.navContainer}>
             
             <Link href="/" className={styles.logoMenu} onClick={() => setToggle(false)}>
-                {/* <Image priority loading='eager' quality={100} src='/logo.svg' width={240.48} height={34.00} alt="logo l'arte d'annodare" /> */}
+                <Image priority loading='eager' quality={100} src='/logo.svg' width={240.48} height={34.00} alt="logo l'arte d'annodare" />
             </Link>
 
                     <ul className={styles.navMenu}>
