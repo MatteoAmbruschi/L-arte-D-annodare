ad8d30c22af7596af823703cfffdc5f7418f3368
Matteo Ambruschi <matteoambruschi10@gmail.com>
2024-03-26 17:00:05 +0100

    menu loading='eager'

--- src/components/menu/Menu.jsx
+++ src/components/menu/Menu.jsx
@@ -34,7 +34,7 @@ const Menu = () => {
             <div className={styles.navContainer}>
             
             <Link href="/" className={styles.logoMenu} onClick={() => setToggle(false)}>
-                <Image src='/logo.svg' width={240.48} height={34.00} alt="logo l'arte d'annodare" priority={true} />
+                <Image loading='eager' src='/logo.svg' width={240.48} height={34.00} alt="logo l'arte d'annodare" />
             </Link>
 
                     <ul className={styles.navMenu}>
