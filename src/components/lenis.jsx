a7b2bd2dc33b4d831a6e1e88f0b936f80b3dc032
Matteo Ambruschi <matteoambruschi10@gmail.com>
2024-04-17 10:54:45 +0200

    scroll to top in chenge page and optional css lenis

--- src/components/lenis.jsx
+++ src/components/lenis.jsx
@@ -1,22 +1,35 @@
 'use client'
-import { ReactLenis } from '@studio-freight/react-lenis';
+import { ReactLenis, useLenis } from '@studio-freight/react-lenis';
 import { useState, useEffect } from 'react'
+import { usePathname } from 'next/navigation';
 
-export default function Lenis({children}) {
+export default function Lenis({ children }) {
     const [isMobile, setIsMobile] = useState(false);
+    const pathname = usePathname();
+
+    const lenis = useLenis(({ scroll }) => {
+    })
 
     useEffect(() => {
         setIsMobile(window.innerWidth <= 768);
     }, []);
 
+    useEffect(() => {
+        if (!isMobile && lenis) {
+            lenis.scrollTo(0, {
+                immediate: true,
+            }); 
+        }
+    }, [pathname]);
+
     return (
         <>
             {isMobile ? (
-                    children
-                ) : (
-                    <ReactLenis root>
-                        {children}
-                    </ReactLenis>
+                children
+            ) : (
+                <ReactLenis root>
+                    {children}
+                </ReactLenis>
             )}
         </>
     )
