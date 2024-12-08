083e74568127ae138f432eb68be1c5d605844323
Matteo <matteoambruschi10@gmail.com>
2024-12-08 18:52:56 +0100

    banner chiusura

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,18 +1,20 @@
 import "./globals.css";
 
 import Menu from "@/components/menu/Menu";
-import Footer from "@/components/footer/Footer"
-import { Analytics } from "@vercel/analytics/react"
-import { SpeedInsights } from '@vercel/speed-insights/next';
+import Footer from "@/components/footer/Footer";
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import Lenis from "../components/lenis";
 import { BannerClose } from "@/components/bannerClose/BannerClose";
 
 export const metadata = {
   title: {
-    default: "L'arte D'annodare | Tappeti orientali Bergamo, lavaggio e restauro a Seriate (Bergamo)",
-    template: "%s | L'arte D'annodare"
+    default:
+      "L'arte D'annodare | Tappeti orientali Bergamo, lavaggio e restauro a Seriate (Bergamo)",
+    template: "%s | L'arte D'annodare",
   },
-  description: "Punto di riferimento a Bergamo e provincia per chi ama il tappeto orientale. Offriamo vendita, restauro e lavaggio tappeti. Servizio di consulenza e preventivi gratuiti.",
+  description:
+    "Punto di riferimento a Bergamo e provincia per chi ama il tappeto orientale. Offriamo vendita, restauro e lavaggio tappeti. Servizio di consulenza e preventivi gratuiti.",
   metadataBase: new URL("https://lartedannodare.it"),
 };
 
@@ -21,14 +23,12 @@ export default function RootLayout({ children }) {
     <html lang="it-IT">
       <body>
         <Menu />
-            <Lenis>
-              {children}
-            </Lenis>
-            {/* <BannerClose />  */} 
-          <Footer />
-              <Analytics />
-              <SpeedInsights />
-        </body>
+        <Lenis>{children}</Lenis>
+        <BannerClose />
+        <Footer />
+        <Analytics />
+        <SpeedInsights />
+      </body>
     </html>
   );
 }
--- src/components/bannerClose/BannerClose.jsx
+++ src/components/bannerClose/BannerClose.jsx
@@ -6,8 +6,12 @@ export const BannerClose = () => {
 
     return (
         <section className={styles.container}>
-            <p>Dal 4/8 al 26/8 chiusura estiva del NEGOZIO ma prosegue l&apos;attività di LABORATORIO.</p>
-            <p className={styles.call}>Per appuntamento:&nbsp;<i><Link href='tel:3297342685'>3297342685</Link></i></p>
+            {/* <p>Dal 4/8 al 26/8 chiusura estiva del NEGOZIO ma prosegue l&apos;attività di LABORATORIO.</p>
+            <p className={styles.call}>Per appuntamento:&nbsp;<i><Link href='tel:3297342685'>3297342685</Link></i></p> */}
+
+            <p>Chiusura definitiva negozio 24/12/24</p>
+            <p className={styles.call}>😀&nbsp;Dal 2/1/2025 operativi presso il nostro nuovo laboratorio:&nbsp;<i><Link href='tel:3297342685'>3297342685</Link></i>&nbsp;😀
+            </p>
         </section>
     )
 }
