eac1ee487a30cb4e6c8335681371841b48c2214c
Matteo <matteoambruschi10@gmail.com>
2025-01-06 20:17:25 +0100

    fix

--- src/components/bannerClose/BannerClose.jsx
+++ src/components/bannerClose/BannerClose.jsx
@@ -9,7 +9,7 @@ export const BannerClose = () => {
             {/* <p>Dal 4/8 al 26/8 chiusura estiva del NEGOZIO ma prosegue l&apos;attività di LABORATORIO.</p>
             <p className={styles.call}>Per appuntamento:&nbsp;<i><Link href='tel:3297342685'>3297342685</Link></i></p> */}
 
-            <p>Chiusura definitiva negozio 24/12/24</p>
+            <p>Chiusura definitiva dell&apos;attività nel negozio 24/12/24</p>
             <p className={styles.call}>😀&nbsp;Dal 2/1/2025 operativi presso il nostro nuovo laboratorio:&nbsp;<i><Link href='tel:3297342685'>3297342685</Link></i>&nbsp;😀
             </p>
         </section>
